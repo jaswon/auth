96135f7b9e1571f8445a46bfc1a7d6c301ca9cf5
jaswon5791 <jasonwon628@gmail.com>
2020-04-04 00:08:55 -0400

    new stack

--- lib/auth-stack.ts
+++ lib/auth-stack.ts
@@ -1,9 +1,25 @@
 import * as cdk from '@aws-cdk/core';
+import * as apigateway from "@aws-cdk/aws-apigateway";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class AuthStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const handler = new lambda.Function(this, "AuthHandler", {
+      runtime: lambda.Runtime.GO_1_X,
+      code: lambda.Code.asset("function"),
+      handler: "main",
+    })
+
+    const api = new apigateway.RestApi(this, "auth-api", {
+      restApiName: "Auth Service",
+      description: "provides auth",
+    })
+
+    const authLambdaIntegration = new apigateway.LambdaIntegration(handler)
+
+    api.root.addMethod("POST", authLambdaIntegration)
+    api.root.addMethod("GET", authLambdaIntegration)
   }
 }
