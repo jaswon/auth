90e368789cbf909dd10a8143b2b252211a450a89
jaswon5791 <jasonwon628@gmail.com>
2020-04-04 02:55:42 -0400

    serve pubkey

--- lib/auth-stack.ts
+++ lib/auth-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as apigateway from "@aws-cdk/aws-apigateway";
 import * as lambda from "@aws-cdk/aws-lambda";
+import * as assets from "@aws-cdk/aws-s3-assets";
+import * as iam from "@aws-cdk/aws-iam";
 
 export class AuthStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -17,9 +19,36 @@ export class AuthStack extends cdk.Stack {
       description: "provides auth",
     })
 
+    const apiRole = new iam.Role(this, "auth-api-role", {
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com")
+    })
+
     const authLambdaIntegration = new apigateway.LambdaIntegration(handler)
 
-    api.root.addMethod("POST", authLambdaIntegration)
-    api.root.addMethod("GET", authLambdaIntegration)
+    api.root.addResource("token").addMethod("POST", authLambdaIntegration)
+
+    const pubkey = new assets.Asset(this, "pubkey", {
+      path: "function/sign.key.pub",
+    })
+
+    pubkey.grantRead(apiRole)
+
+    const pubkeyIntegration = new apigateway.AwsIntegration({
+      service: "s3",
+      path: `${pubkey.s3BucketName}/${pubkey.s3ObjectKey}`,
+      integrationHttpMethod: "GET",
+      options: {
+        credentialsRole: apiRole,
+        integrationResponses: [
+          { statusCode: "200" },
+        ],
+      }
+    })
+
+    api.root.addResource("pubkey").addMethod("GET", pubkeyIntegration, {
+      methodResponses: [
+        { statusCode: "200" },
+      ],
+    })
   }
 }
