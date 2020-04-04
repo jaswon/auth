9cdd2ab82c54dc803e0b747bc290d4d5de734f7d
jaswon5791 <jasonwon628@gmail.com>
2020-04-04 03:27:54 -0400

    read private key in lambda

--- function/go.mod
+++ function/go.mod
@@ -2,4 +2,7 @@ module function
 
 go 1.14
 
-require github.com/aws/aws-lambda-go v1.16.0
+require (
+	github.com/aws/aws-lambda-go v1.16.0
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+)
--- function/main.go
+++ function/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"crypto/rsa"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/dgrijalva/jwt-go"
 )
+
+var signKey *rsa.PrivateKey
+
+func init() {
+	pf, _ := ioutil.ReadFile("sign.key")
+	signKey, _ = jwt.ParseRSAPrivateKeyFromPEM(pf)
+}
 
 func HandleRequest(ev events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       fmt.Sprintf("method=%s path=%s resource=%s body=%s", ev.HTTPMethod, ev.Path, ev.Resource, ev.Body),
+		Body:       fmt.Sprintf("method=%s path=%s body=%s", ev.HTTPMethod, ev.Path, ev.Body),
 	}, nil
 }
 
